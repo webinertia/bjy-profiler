e16956a0634d33d1b095133436a13d9c1517490d
Joey Smith <jsmith@webinertia.net>
2024-02-09 17:57:42 -0600

    Update ProfilingAdapterFactory.php

--- src/Db/Adapter/ProfilingAdapterFactory.php
+++ src/Db/Adapter/ProfilingAdapterFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace BjyProfiler\Db\Adapter;
 
 use BjyProfiler\Db\Profiler;
-use Laminas\Log;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerInterface;
 
@@ -18,11 +17,11 @@ final class ProfilingAdapterFactory implements FactoryInterface
         $adapter = new ProfilingAdapter($container->get('config')['db']);
 
         if ('cli' === PHP_SAPI) {
-            $logger = new Log\Logger();
-            // write queries profiling info to stdout in CLI mode
-            $writer = new Log\Writer\Stream('php://output');
-            $logger->addWriter($writer, Log\Logger::DEBUG);
-            $adapter->setProfiler(new Profiler\LoggingProfiler($logger));
+            // $logger = new Log\Logger();
+            // // write queries profiling info to stdout in CLI mode
+            // $writer = new Log\Writer\Stream('php://output');
+            // $logger->addWriter($writer, Log\Logger::DEBUG);
+            // $adapter->setProfiler(new Profiler\LoggingProfiler($logger));
         } else {
             $adapter->setProfiler(new Profiler\Profiler());
         }
